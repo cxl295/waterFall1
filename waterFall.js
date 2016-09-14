fb901a765dfcec2e100a8b713b4eb5be08616ddf
cxl295 <178473387@qq.com>
2016-09-14 10:54:01 +0800

    Create waterFall.js

---
+++ waterFall.js
@@ -0,0 +1,35 @@
+/**
+ * Created by admin on 2016/9/14.
+ */
+window.onload=function(){
+
+    waterfall('main','pin');
+}
+
+/*
+ parent 父级id
+ pin 元素id
+ */
+function waterfall(parent,pin){
+    var oParent=document.getElementById(parent);// 父级对象
+    var aPin=getClassObj(oParent,pin);// 获取存储块框pin的数组aPin
+    var iPinW=aPin[0].offsetWidth;// 一个块框pin的宽
+    var num=Math.floor(document.documentElement.clientWidth/iPinW);//每行中能容纳的pin个数【窗口宽度除以一个块框宽度】
+    oParent.style.cssText='width:'+iPinW*num+'px;margin:0 auto;';//设置父级居中样式：定宽+自动水平外边距
+
+}
+
+/*
+ *通过父级和子元素的class类 获取该同类子元素的数组
+ */
+function getClassObj(parent,className){
+    var obj=parent.getElementsByTagName('*');//获取 父级的所有子集
+    var pinS=[];//创建一个数组 用于收集子元素
+    for (var i=0;i<obj.length;i++) {//遍历子元素、判断类别、压入数组
+        if (obj[i].className==className){
+            pinS.push(obj[i]);
+        }
+    };
+    return pinS;
+
+}
